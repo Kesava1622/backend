2440cbcbb772f1979c1ed2617c80314297b3373f
kesava1622 <kesavakumarmtsa1622@gmail.com>
2025-06-10 19:45:07 +0530

    check 1

--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 const app= require('cors');
+app.use(cors());
 
 
 // Get all products by category
