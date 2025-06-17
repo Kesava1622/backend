516f6b028294c96bd44097409acc782310570c02
kesava1622 <kesavakumarmtsa1622@gmail.com>
2025-06-17 12:57:58 +0530

    ch

--- db.js
+++ db.js
@@ -15,7 +15,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 1000,
   queueLimit: parseInt(process.env.DB_QUEUE_LIMIT) || 0,
